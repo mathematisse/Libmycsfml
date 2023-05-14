4a1d6fb68e0535b26c85888b12b0e3dad3d8baa5
mathematisse <lucien.pineau@epitech.eu>
2023-05-14 13:45:59 +0200

    [EDIT] primitive rect_set

--- src/panel/panel_resize.c
+++ src/panel/panel_resize.c
@@ -13,39 +13,40 @@
 static void sprite_rect_set(panel_t *panel)
 {
     sfVector2f sprite_pos = {0, 0};
-    sfFloatRect sprite_size = sfSprite_getGlobalBounds(panel->sprite);
+    sfFloatRect sprite_size = sfSprite_getGlobalBounds(panel->data);
 
     sprite_pos.x = panel->pos.x + (panel->size.x - sprite_size.width) / 2;
     sprite_pos.y = panel->pos.y + (panel->size.y - sprite_size.height) / 2;
-    sfSprite_setPosition(panel->sprite, sprite_pos);
+    sfSprite_setPosition(panel->data, sprite_pos);
 }
 
 static void text_rect_set(panel_t *panel, float scale)
 {
     sfFloatRect trect;
 
-    sfText_setCharacterSize(panel->text, (unsigned int) 20.0 * scale);
-    trect = sfText_getLocalBounds(panel->text);
-    sfText_setOrigin(panel->text, (sfVector2f){
+    sfText_setCharacterSize(panel->data, (unsigned int) 20.0 * scale);
+    trect = sfText_getLocalBounds(panel->data);
+    sfText_setOrigin(panel->data, (sfVector2f){
         trect.left + trect.width / 2.0f, panel->size.y / 2.0f});
-    sfText_setPosition(panel->text, panel->pos);
+    sfText_setPosition(panel->data, panel->pos);
+}
+
+static void shape_rect_set(panel_t *panel)
+{
+    sfRectangleShape_setSize(panel->data, panel->size);
+    sfRectangleShape_setOrigin(panel->data,
+        (sfVector2f){panel->size.x / 2, panel->size.y / 2});
+    sfRectangleShape_setPosition(panel->data, panel->pos);
 }
 
 static void panel_rect_set(panel_t *panel, float scale)
 {
     if (!panel)
         return;
-    if (panel->shape) {
-        sfRectangleShape_setSize(panel->shape, panel->size);
-        sfRectangleShape_setOrigin(panel->shape,
-            (sfVector2f){panel->size.x / 2, panel->size.y / 2});
-        sfRectangleShape_setPosition(panel->shape, panel->pos);
-    }
-    if (panel->sprite) {
-        sprite_rect_set(panel);
-    }
-    if (panel->text) {
-        text_rect_set(panel, scale);
+    switch (panel->type) {
+        case PANEL_T_SHAPE: shape_rect_set(panel); break;
+        case PANEL_T_SPRITE: sprite_rect_set(panel); break;
+        case PANEL_T_TEXT: text_rect_set(panel, scale); break;
     }
 }
 
